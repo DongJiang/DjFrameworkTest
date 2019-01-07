749e2df373839e298b9bc7332bf6fb3625be4862
Jiang Dong <jiang.dong@aalto.fi>
2019-01-07 10:15:34 +0200

    test framework

---
+++ DjFrameworkTest/DjFrameworkTest/DjFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  DjFrameworkTest.h
+//  DjFrameworkTest
+//
+//  Created by Jiang Dong on 07/01/2019.
+//  Copyright © 2019 vimai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DjFrameworkTest.
+FOUNDATION_EXPORT double DjFrameworkTestVersionNumber;
+
+//! Project version string for DjFrameworkTest.
+FOUNDATION_EXPORT const unsigned char DjFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DjFrameworkTest/PublicHeader.h>
+
+
---
+++ DjFrameworkTest/DjFrameworkTest/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  DjFrameworkTest
+//
+//  Created by Jiang Dong on 07/01/2019.
+//  Copyright © 2019 vimai. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    private init(){
+        
+    }
+    
+    public static func getString() -> String{
+        return "String"
+    }
+}
