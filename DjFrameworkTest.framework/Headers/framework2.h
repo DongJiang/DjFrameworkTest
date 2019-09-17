aad26ffe0d0807a65124ad18b0649284171ace81
Jiang Dong <jiang.dong@vim.ai>
2019-09-17 10:39:02 +0300

    test different framework

---
+++ DjFrameworkTest.framework/Headers/framework2.h
@@ -0,0 +1,19 @@
+//
+//  framework2.h
+//  framework2
+//
+//  Created by Jiang Dong on 17/09/2019.
+//  Copyright © 2019 Jiang Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for framework2.
+FOUNDATION_EXPORT double framework2VersionNumber;
+
+//! Project version string for framework2.
+FOUNDATION_EXPORT const unsigned char framework2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework2/PublicHeader.h>
+
+
