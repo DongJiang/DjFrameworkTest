dadbd6130eb8f650f1e3a75adedc2790754685be
Jiang Dong <jiang.dong@vim.ai>
2019-09-17 10:19:48 +0300

    1.0.5

---
+++ DjFrameworkTest.framework/Headers/DjFrameworkTest.h
@@ -0,0 +1,20 @@
+//
+//  DjFrameworkTest.h
+//  DjFrameworkTest
+//
+//  Created by Jiang Dong on 13/09/2019.
+//  Copyright © 2019 Jiang Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <DjFrameworkTest/wrapper.h>
+
+//! Project version number for DjFrameworkTest.
+FOUNDATION_EXPORT double DjFrameworkTestVersionNumber;
+
+//! Project version string for DjFrameworkTest.
+FOUNDATION_EXPORT const unsigned char DjFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DjFrameworkTest/PublicHeader.h>
+
+
